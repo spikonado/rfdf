b0c35d2193bcded67965d248a5237cda26b8d92a
Aarav Gupta <aarav@spikonado.com>
2026-04-11 17:20:34 +0530

    build(nix): Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs"; # IMPORTANT!!!
+  };
+  outputs =
+    {
+      self,
+      nix-ros-overlay,
+      nixpkgs,
+    }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+
+        rosDistro = "kilted";
+
+        rosPkgs = pkgs.rosPackages.${rosDistro};
+
+        devTools = with pkgs; [
+          commitlint
+          bun
+          gcc
+          gh
+          git
+          git-lfs
+          prek
+          prettier
+        ];
+
+        # Define the ROS environment with all necessary dependencies
+        rosEnv = rosPkgs.buildEnv {
+          wrapPrograms = false;
+          paths =
+            with pkgs;
+            with rosPkgs;
+            [
+              colcon
+              ros-core
+              ament-lint-common
+
+              # Work around https://github.com/lopsided98/nix-ros-overlay/pull/624
+              ament-cmake-core
+              python-cmake-module
+
+              # Dependencies from package.xml files
+              ament-cmake
+            ];
+        };
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
+        legacyPackages = rosPkgs;
+
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          name = "rfdf-shell";
+          packages = [
+            devTools
+            rosEnv
+          ];
+
+          shellHook = ''
+            # Setup ROS 2 shell completion
+            if [[ ! $DIRENV_IN_ENVRC ]]; then
+                eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete ros2)"
+                eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete colcon)"
+            fi
+          '';
+        };
+      }
+    );
+  nixConfig = {
+    extra-substituters = [
+      "https://spikonado.cachix.org"
+      "https://ros.cachix.org"
+      "https://attic.iid.ciirc.cvut.cz/ros"
+    ];
+    extra-trusted-public-keys = [
+      "spikonado.cachix.org-1:MwA4hqRN0+DdP7/UnTn0yvJgVu65S1S0QVnAnsguev4="
+      "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
+      "ros:JR95vUYsShSqfA1VTYoFt1Nz6uXasm5QrcOsGry9f6Q="
+    ];
+  };
+}
